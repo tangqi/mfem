4dbf4b0d51f3d467b79f903b327a9c3a1ec94715
Michael Schneier <schneimh@unnpp.gov>
2020-12-12 16:43:56 -0500

    debugging

--- fem/bilininteg.cpp
+++ fem/bilininteg.cpp
@@ -1119,7 +1119,7 @@ void VectorConvectionIntegrator::AssembleElementMatrix(
       {
          for (int l = 0; l <nd; l++)
          {
-            elmat(nd*d+k, nd*d+l) = pelmat(k, l);
+            elmat(nd*d+k, nd*d+l) = 10000*pelmat(k, l);
             cout << elmat(nd*d+k, nd*d+l) << endl;
          }
       }
